ab04020b34ce88a66f5a8474ad40e21d492200e4
Andrey Vasilev <philobeat@gmail.com>
2025-01-29 14:53:54 +0300

    feature: Added an IMEI checker service

---
+++ api/utils.py
@@ -0,0 +1,29 @@
+import httpx
+
+from config import settings
+from schemas import IMEICheckResponse
+
+
+async def check_imei(device_id: str, service_id: int = 12) -> IMEICheckResponse:
+    url = "https://api.imeicheck.net/v1/checks"
+    headers = {
+        "Authorization": f"Bearer {settings.imeicheck_api_sandbox_token}",
+        "Accept-Language": "en",
+        "Content-Type": "application/json",
+    }
+    payload = {
+        "deviceId": device_id,
+        "serviceId": service_id,
+    }
+
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.post(url=url, json=payload, headers=headers)
+            response.raise_for_status()
+            return IMEICheckResponse.model_validate(response.json())
+        except httpx.HTTPStatusError as e:
+            error_details = f"Cannot proccess the request: {e.response.status_code} - {e.response.text}"
+
+            raise ValueError(error_details)
+        except Exception as e:
+            raise ValueError(f"Ошибка при проверке IMEI: {str(e)}")
