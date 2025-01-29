9d74bf8fe00067d4b3b34054e58219a45bdbe53e
Andrey Vasilev <philobeat@gmail.com>
2025-01-29 15:10:41 +0300

    refactor: Corrected return

--- api/endpoints.py
+++ api/endpoints.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import APIKeyHeader
 
 from config import settings
-from schemas import IMEICheckResponse
+from schemas import IMEICheckResponse, IMEICheckCreateRequest
 from utils import check_imei
 
 api_router = APIRouter()
@@ -15,8 +15,10 @@ async def authenticate(token: str = Depends(security)) -> bool:
 
 
 @api_router.post("/check-imei", response_model=IMEICheckResponse)
-async def check_imei_endpoint(imei: str, auth: bool = Depends(authenticate)):
+async def check_imei_endpoint(request: IMEICheckCreateRequest, auth: bool = Depends(authenticate)):
     if not auth:
         raise HTTPException(status_code=403, detail="Invalid token")
     try:
-        await check_imei(device_id=imei)
+        return await check_imei(request.device_id, request.service_id)
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
