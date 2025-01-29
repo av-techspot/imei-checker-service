d8b336307f1fdc8566bdae21ce7437612ed085a2
Andrey Vasilev <philobeat@gmail.com>
2025-01-29 12:27:11 +0300

    feature: Added a config module

---
+++ config.py
@@ -0,0 +1,26 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    api_host: str = "0.0.0.0"
+    api_port: int = 8040
+
+    imeicheck_api_sandbox_token: str
+    imeicheck_api_live_token: str
+
+    bot_token: str
+
+    db_host: str
+    db_port: int
+    db_user: str
+    db_pass: str
+    db_name: str
+
+    model_config = SettingsConfigDict(env_file=".env", case_sensitive=False)
+
+    @property
+    def database_url(self):
+        return f"postgresql+asyncpg://{self.db_user}:{self.db_pass}@{self.db_host}:{self.db_port}/{self.db_name}"
+
+
+settings = Settings()
